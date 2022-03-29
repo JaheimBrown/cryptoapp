a164fea0521b87e60668f86e790a4d6d9b4e6bf1
kxngj <jaheimcbrown@gmail.com>
2022-03-29 11:20:09 -0500

    Added cryptocurrencies page with filterd data and search functionality

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+  "X-RapidAPI-Key": "6775d69c52msh1d67ce1c9fc80d4p13571fjsn426e7baa47aa",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = url => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: builder => ({
+    getCryptos: builder.query({
+      query: count => createRequest(`/coins?limit=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
