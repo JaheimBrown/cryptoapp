72877a015ae368727295bd9d5e2b6c67ae8bfd14
kxngj <jaheimcbrown@gmail.com>
2022-03-31 00:02:32 -0500

    Added environmental variables and added favicon to project

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const cryptoNewsApiHeaders = {
   "X-BingApis-SDK": "true",
   "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com",
-  "X-RapidAPI-Key": "6775d69c52msh1d67ce1c9fc80d4p13571fjsn426e7baa47aa",
+  "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
 };
 
 const baseUrl = "https://bing-news-search1.p.rapidapi.com/news/search";
